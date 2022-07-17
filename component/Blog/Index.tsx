ede2adf0c2a2368a7d3eab8fd2468a41382d92b4
lawsondaniel <lawblaze4@gmail.com>
2022-07-17 21:02:31 +0100

    done with the second section of the blog

--- component/Blog/Index.tsx
+++ component/Blog/Index.tsx
@@ -20,6 +20,25 @@ const Index = () => {
                     <div className={Style.HeaderH1}>Articles and News</div>
                 </div>
             </section>
+            <section className={Style.session2}>
+                    <div className={Style.left}>
+                        <h1>Latest From The Blog</h1>
+                        <div>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero 
+                            eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+                            takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur 
+                            </div>
+                    </div>
+                    <div className={Style.right}>
+                        <div className={Style.form}>
+                            <input type="text" placeholder="Search here" />
+                            <span className="material-symbols-outlined">
+                            search
+                            </span>
+
+                        </div>
+                    </div>
+            </section>
         <div className={Style.footercontainer}>
             <Footerlight/>
         </div>
