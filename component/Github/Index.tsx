7f760e8691746484b9dea8d81ca4ad4695dcb636
lawsondaniel <lawblaze4@gmail.com>
2022-07-18 10:49:06 +0100

    consumed the api from git hub

---
+++ component/Github/Index.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Style from '../../styles/github.module.css'
+
+const Index = () => {
+    //empty state to setdat 
+    const [data,setdata] = useState([])
+   
+    
+    // get days 
+    const getDifferenceInDays = (now: any, dateCreated: any) => {
+        let previousDate: any = new Date(dateCreated);
+        const diffInMs = Math.abs(now - previousDate);
+        return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+    };
+    
+    useEffect(()=>{
+        axios.get(`https://api.github.com/search/repositories?q=created:>2022-04-13&sort=stars&order=desc`).then((res)=>{
+            setdata(res.data.items)
+        }).catch((err)=>{
+              console.error(err)
+        })
+    },[])
+   
+    let Now = new Date();
+   
+  return (
+    <div className={Style.container}>
+        
+      {
+        data.map((a:any)=>{
+            return (
+            <>
+                <div className={Style.card}>
+                    <div className={Style.img}>
+                        <img src={a.owner.avatar_url} alt="img"/>
+                    </div>
+                    <div className={Style.info}>
+                        <h1>{a.name}</h1>
+                        <p>{a.description}</p>
+                        <div className={Style.star}>
+                        <div className={Style.starcontainer}>Stars: {a.stargazers_count}</div>
+                        <div className={Style.starcontainer}>Issue: {a.open_issues_count }</div>
+                    <div>Submitted {getDifferenceInDays(Now, a.created_at)} days ago by {a.full_name.split("/")[0]} </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+            )
+        })
+      }
+     
+    </div>
+  )
+}
+
+export default Index
+
+
---
+++ pages/github.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Index from '../component/Github/Index'
+
+const github = () => {
+  return (
+    <div><Index/></div>
+  )
+}
+
+export default github
